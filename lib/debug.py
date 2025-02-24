78e10970d149d2406f10d9c5f717310d4b48efb0
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-24 13:36:52 -0500

    Added example code from the lecture

--- lib/debug.py
+++ lib/debug.py
@@ -7,4 +7,19 @@ from classes.many_to_many import Review
 
 if __name__ == '__main__':
     # don't remove this line, it's for debugging!
+
+    # Hotels
+    marriott = Hotel("Marriott")
+    the_chanler_at_cliff_walk = Hotel("Chanler Hotel")
+
+    # Customers
+    alice_baker = Customer("Alice", "Baker")
+    bob_carris = Customer("Bob", "Carris")
+
+    # Reviews
+    review_1 = Review(marriott, alice_baker, 4, "Pretty awesome amenities here!")
+    review_2 = Review(the_chanler_at_cliff_walk, alice_baker, 5, "Best hotel ever!")
+    review_3 = Review(marriott, alice_baker, 3, "Not as great as my first visit.")
+    review_4 = Review(marriott, bob_carris, 5, "Amazing!")
+
     ipdb.set_trace()
